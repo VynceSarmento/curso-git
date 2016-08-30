7166fdc0b83eb8ea33e241e3441b6127f2d8b497
Vinicius Sarmento Costa Siqueira <vinicius.eng.comp92@gmail.com>
2016-08-29 21:49:09 -0300

    Arrumando Código

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
         <title>Curso Git</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="estilo.css">
     </head>
     <body>
         <h1>Curso Git</h1>
